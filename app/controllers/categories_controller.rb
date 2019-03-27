163f3bddf14b1656331b96b3fde5276ba716f7e7
Takagi <uruhuodoneru2002@gmail.com>
2019-03-27 11:17:22 +0000

    カテゴリーに纏わるコントローラー記述

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,16 @@
 class CategoriesController < ApplicationController
 	@categories = Category.eager_load(children: :children).where(parent_id: nil)
+
+    def new
+    end
+
+    def create
+    end
+
+    def index
+    end
+
+    def show
+    end
+
 end
--- app/controllers/post_helps_controller.rb
+++ app/controllers/post_helps_controller.rb
@@ -1,2 +1,15 @@
 class PostHelpsController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+
+    def index
+    end
+
+    def show
+    end
+
 end
